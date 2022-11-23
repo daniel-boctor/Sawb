afb7db08da8eb73325e3870c7e96d2c7ed658598
daniel-boctor <dboctor18@gmail.com>
2022-11-22 23:37:40 -0500

    Sawb.py created

---
+++ Sawb.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-e', '--extention',  default='txt', type=str, help='Specificy the file type you would like to index.')
+parser.add_argument('-d', '--delimiter',  default=' ', type=str, help='Specificy the delimiter for the file names.')
+parser.add_argument('-o', '--output',  default='index', type=str, help='Specificy the file name you would like to write to.')
+parser.add_argument('-t', '--output_type',  default='txt', type=str, help='Specificy weather the output file should be a txt or a csv.')
+parser.add_argument('-f', '--directory_path',  default='.', type=str, help='Specificy the directory to index from.')
+parser.add_argument('-s', '--save_path',  default='', type=str, help='Specificy the save location.')
+
+args = parser.parse_args()
+
+raw_file_names = [f for f in os.listdir(args.directory_path) if os.path.isfile(os.path.join(args.directory_path, f)) and f.endswith(args.extention)]
+
+with open(os.path.join(args.save_path, f"{args.output}.{args.output_type}"), "w") as output:
+    for file in raw_file_names: output.write(file.split(args.delimiter)[0] + ('\n' if args.output_type=="txt" else ','))
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Sawb.py will create an index of the head of each filename in a directory
+
+### Usage
+
+| Arg            | Shorthand   | Default | Description
+| -----------    | ----------- | ------- | -----------
+| extention      | e           | txt     | Specificy the file type you would like to index.
+| delimiter      | d           | ' '     | Specificy the delimiter for the file names.
+| output         | o           | index   | Specificy the file name you would like to write to.
+| output_type    | t           | txt     | Specificy weather the output file should be a txt, or a csv.
+| directory_path | f           | CWD     | Specificy the directory to index from.
+| save_path      | s           | CWD     | Specificy the save location.
